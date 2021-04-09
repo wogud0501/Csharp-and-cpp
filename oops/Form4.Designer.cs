6559f20a50d1fb72651fbe27c5f327fd1eb5d6b3
wogud0501 <djaeyeo@gmail.com>
2021-04-09 10:05:35 +0900

    oops

---
+++ oops/Form4.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace oops
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plBack = new System.Windows.Forms.Panel();
+            this.picClose = new System.Windows.Forms.PictureBox();
+            this.plBack.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picClose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // plBack
+            // 
+            this.plBack.BackColor = System.Drawing.Color.LightBlue;
+            this.plBack.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.plBack.Controls.Add(this.picClose);
+            this.plBack.Location = new System.Drawing.Point(0, 0);
+            this.plBack.Name = "plBack";
+            this.plBack.Size = new System.Drawing.Size(200, 100);
+            this.plBack.TabIndex = 0;
+            // 
+            // picClose
+            // 
+            this.picClose.Image = global::oops.Properties.Resources._20210409_000033;
+            this.picClose.Location = new System.Drawing.Point(0, 0);
+            this.picClose.Name = "picClose";
+            this.picClose.Size = new System.Drawing.Size(100, 50);
+            this.picClose.TabIndex = 0;
+            this.picClose.TabStop = false;
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(170, 0);
+            this.Controls.Add(this.plBack);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form4";
+            this.Opacity = 0D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.plBack.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picClose)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel plBack;
+        private System.Windows.Forms.PictureBox picClose;
+    }
+}
---
+++ oops/Form4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oops
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            int x = Screen.PrimaryScreen.WorkingArea.Width - this.Width - 20; // 스크린의 가로 위치
+            int y = Screen.PrimaryScreen.WorkingArea.Height - this.Height; // 스크린의 세로 위치
+
+            DesktopLocation = new Point(x, y); // 폼의 위치 설정
+            InitializeComponent();
+        }
+        private static System.Timers.Timer TimerEvent; // 타이머 객체 생성
+
+        private void PicClose_Click(object sender, EventArgs e)
+    }
+   
+}
