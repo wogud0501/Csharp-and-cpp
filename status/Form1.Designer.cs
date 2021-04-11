8c55cb724b1409b2dc04073c72cfddc408a59ec4
wogud0501 <djaeyeo@gmail.com>
2021-04-11 13:18:39 +0900

    status

---
+++ status/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace status
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plBar = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // plBar
+            // 
+            this.plBar.BackColor = System.Drawing.Color.White;
+            this.plBar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.plBar.Location = new System.Drawing.Point(12, 12);
+            this.plBar.Name = "plBar";
+            this.plBar.Size = new System.Drawing.Size(420, 34);
+            this.plBar.TabIndex = 0;
+            this.plBar.Paint += new System.Windows.Forms.PaintEventHandler(this.plBar_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(444, 79);
+            this.Controls.Add(this.plBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "메모리 체크";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel plBar;
+    }
+}
+
---
+++ status/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+
+namespace status
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            checkThread = new Thread(getCPU_Info);
+            checkThread.Start();   // checkThread 스레드 프로세스 시작
+        }
+        private PerformanceCounter oCPU =
+            new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        // 시스템 성능 카운터
+        private bool bExit = false;               // 실시간 체크를 위한 while 조건
+        private int iCPU = 0;                     // cpu 초기 사용률
+        private Font F = new Font("굴림", 9);     // 폰트 모양
+        private Thread checkThread;               // 스레드 개체 생성
+
+
+        private void getCPU_Info()
+        {
+            while (!bExit)
+            {
+                iCPU = (int)oCPU.NextValue();
+                this.Text = "CPU 사용: " + iCPU.ToString() + "%";
+                iCPU = iCPU * 3;
+                plBar.Invalidate();
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void plBar_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics G = e.Graphics;
+            if (iCPU <= 60)
+                G.FillRectangle(Brushes.BlanchedAlmond, 0, 0, iCPU, plBar.Height);
+            else if (iCPU <= 120)
+                G.FillRectangle(Brushes.Wheat, 0, 0, iCPU, plBar.Height);
+            else if (iCPU <= 180)
+                G.FillRectangle(Brushes.NavajoWhite, 0, 0, iCPU, plBar.Height);
+            else if (iCPU <= 240)
+                G.FillRectangle(Brushes.Orange, 0, 0, iCPU, plBar.Height);
+            else
+                G.FillRectangle(Brushes.DarkOrange, 0, 0, iCPU, plBar.Height);
+            iCPU = iCPU / 3;
+            G.DrawString(iCPU.ToString() + "%", F, Brushes.DarkRed,
+                plBar.Width / 2 - 17, plBar.Height / 4);
+        }
+    }
+}
