50dcea920db25a778fc5a804046054c3168008d0
Git <pjh52338@naver.com>
2022-02-16 21:21:57 +0900

    Net Stat Source Code Update

--- NetStat/NetStat/Form1.cs
+++ NetStat/NetStat/Form1.cs
@@ -7,11 +7,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.IO;
 
 namespace NetStat
 {
     public partial class Form1 : Form
     {
+        IPGlobalProperties ipProperites =
+            IPGlobalProperties.GetIPGlobalProperties();
+        Thread NetThread = null;
+        string LocPort, RemoAdd, RemoPort;
+        bool CheckBool = true;
+        // 델리게이트 선언
+        private delegate void OnConnViewDelegate(bool flags, string[] AddInfo);
+        // 델리게이트 개체 생성
+        private OnConnViewDelegate OnView = null;
+
         public Form1()
         {
             InitializeComponent();
