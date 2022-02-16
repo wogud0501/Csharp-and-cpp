1a245795a413fe81a332c4ae390ec215755856ee
Git <pjh52338@naver.com>
2022-02-16 19:56:19 +0900

    DnsLookUp Source Code Update

--- DnsLookUp/DnsLookUp/Form1.Designer.cs
+++ DnsLookUp/DnsLookUp/Form1.Designer.cs
@@ -29,13 +29,87 @@ namespace DnsLookUp
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblUrl = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblAdd = new System.Windows.Forms.Label();
+            this.listAddr = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblUrl
+            // 
+            this.lblUrl.AutoSize = true;
+            this.lblUrl.Location = new System.Drawing.Point(29, 51);
+            this.lblUrl.Name = "lblUrl";
+            this.lblUrl.Size = new System.Drawing.Size(61, 12);
+            this.lblUrl.TabIndex = 0;
+            this.lblUrl.Text = "도 메 인 : ";
+            this.lblUrl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblUrl.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(107, 48);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(329, 21);
+            this.txtHost.TabIndex = 1;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(489, 48);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(71, 21);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "검색";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lblAdd
+            // 
+            this.lblAdd.AutoSize = true;
+            this.lblAdd.Location = new System.Drawing.Point(29, 112);
+            this.lblAdd.Name = "lblAdd";
+            this.lblAdd.Size = new System.Drawing.Size(67, 12);
+            this.lblAdd.TabIndex = 3;
+            this.lblAdd.Text = "IP Address";
+            // 
+            // listAddr
+            // 
+            this.listAddr.FormattingEnabled = true;
+            this.listAddr.ItemHeight = 12;
+            this.listAddr.Location = new System.Drawing.Point(31, 127);
+            this.listAddr.Name = "listAddr";
+            this.listAddr.Size = new System.Drawing.Size(529, 100);
+            this.listAddr.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(604, 281);
+            this.Controls.Add(this.listAddr);
+            this.Controls.Add(this.lblAdd);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.lblUrl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "DNS LookUp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblUrl;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblAdd;
+        private System.Windows.Forms.ListBox listAddr;
     }
 }
 
--- DnsLookUp/DnsLookUp/Form1.cs
+++ DnsLookUp/DnsLookUp/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
 
 namespace DnsLookUp
 {
@@ -15,6 +16,42 @@ namespace DnsLookUp
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string HostName = null;
+            if (this.txtHost.Text.Contains("://") == true)
+            {
+                HostName = this.txtHost.Text.Replace("https://", "");
+            }
+            else { HostName = this.txtHost.Text; }
+            try
+            {
+                IPHostEntry ipe = Dns.GetHostEntry(HostName);
+                IPAddress[] addrs = ipe.AddressList;
+                if (listAddr.Items.Count > 0)
+                    listAddr.Items.Clear();
+                foreach (IPAddress addr in addrs)
+                {
+                    listAddr.Items.Add(addr);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "에러",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
     }
 }
