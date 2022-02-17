b56fb646d1cae2d91bf39a254bce4dcfd8eebbaa
Git <pjh52338@naver.com>
2022-02-17 11:38:24 +0900

    Wifi Scanner Source Code Update

--- Wifi Scanner/Wifi Scanner/Form1.cs
+++ Wifi Scanner/Wifi Scanner/Form1.cs
@@ -14,6 +14,12 @@ namespace Wifi_Scanner
 {
     public partial class Form1 : Form
     {
+        WlanClient wlanClient = new WlanClient();
+        Thread thrAP;
+        // 델리게이트 선언
+        private delegate void OnWifiViewDelegate(bool flags, object[] AddWifi);
+        //델리게이트 개체 생성
+        private OnWifiViewDelegate OnView = null;
         public Form1()
         {
             InitializeComponent();
@@ -23,5 +29,94 @@ namespace Wifi_Scanner
         {
 
         }
+        private void ThreadList()
+        {
+            while (true)
+            {
+                OnView.Invoke(true, null);
+                Wlan.WlanAvailableNetwork[] wlanBssEntries =
+                    wlanClient.Interfaces[0].GetAvailableNetworkList(0);
+                foreach (Wlan.WlanAvailableNetwork network in wlanBssEntries)
+                {
+                    object[] AddWifi = new object[7]
+                    {
+                       network.dot11Ssid,
+                       network.wlanSignalQuality.ToString(),
+                       network.securityEnabled.ToString(),
+                       network.dot11Ssid.SSID,
+                       network.dot11DefaultCipherAlgorithm.ToString(),
+                       network.dot11DefaultAuthAlgorithm.ToString(),
+                       network.dot11Ssid.SSID };
+                    OnView.Invoke(false, AddWifi);
+
+                }
+                Thread.Sleep(10000);
+            }
+        }
+        private void OnWifiList(bool flags, object[] AddWifi)
+        {
+            if (flags == true)
+                this.lvAP.Items.Clear();
+            else
+            {
+                var lvt = new ListViewItem(new string[]
+                {
+                    GetStringForSSID((Wlan.Dot11Ssid)AddWifi[0]),
+                    AddWifi[1].ToString(),
+                    AddWifi[2].ToString(),
+                    GetMacChannel(1, ConvertToMAC((byte[])AddWifi[6])) });
+                this.lvAP.Items.Add(lvt);
+            }
+        }
+        private void GetMacChannel(int i, string Name)
+        {
+            Wlan.WlanBssEntry[] IstWlanBss =
+                wlanClient.Interfaces[0].GetNetworkBssList();
+            var reAP = "";
+            foreach (var oWlan in IstWlanBss)
+            {
+                if (i == 2)
+                {
+                    if (ConvertToMAC(oWlan.dot11Ssid.SSID) == Name)
+                    {
+                        reAP = ConvertToMAC(oWlan.dot11Bssid);
+                    }
+                }
+                else if (i == 1)
+                {
+                    if(ConvertToMAC(oWlan.dot11Ssid.SSID) == Name)
+                    {
+                        var chnl = oWlan.chCenterFrequency.ToString();
+                        switch (chnl)
+                        {
+                            case "2412000":
+                                reAP = "1";
+                                break;
+                            case "2417000":
+                                reAP = "2";
+                                break;
+                            case "2422000":
+                                reAP = "3";
+                                break;
+                            case "2427000":
+                                reAP = "4";
+                                break;
+                            case "2432000":
+                                reAP = "5";
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private string ConvertToMAC(byte[] MAC)
+        {
+            string strMAC = "";
+            for (int index = 0; index < 6; index++)
+                strMAC += MAC[index].ToString("X2") + "-";
+            return strMAC.Substring(0, strMAC.Length - 1);
+        }
+        
     }
 }
