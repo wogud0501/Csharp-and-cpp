9a2b41da290080e0bbc0d79b7d7ec97468f7f1eb
Jae-Hyoung Park <djaeyeo@gmail.com>
2022-03-04 23:03:21 +0900

    C++  Update

---
+++ cpp/test01/test01/test01.cpp
@@ -0,0 +1,14 @@
+﻿// test01.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <numeric>
+#include <iterator>
+
+
+int main()
+{
+	double data[]{ 2.5, 4.5, 6.5, 5.5, 8.5 };
+	std::cout << "배열 원소들:\n";
+	for (auto )
+}
