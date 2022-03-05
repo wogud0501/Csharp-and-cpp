5d1cc47bb39f796c4631a17a1dea8c8d966aa1c1
Jae-Hyoung Park <djaeyeo@gmail.com>
2022-03-05 14:37:41 +0900

    C++ Code Update

--- cpp/c++ function/c++ function/c++ function.cpp
+++ cpp/c++ function/c++ function/c++ function.cpp
@@ -17,9 +17,21 @@ int main()
     double data[]{ 1.5, 2.5, 3.5, 4.5, 5.5 };
 
     // 함수 객체를 전달하기
-    Root root;
+    Root root;                          // 함수 객체
     std::cout << "제곱근은 : " << std::endl;
     std::transform(std::begin(data), std::end(data),
         std::ostream_iterator<double>(std::cout, " "), root);
+
+    // 람다 표현식을 인수로 사용하기
+    std::cout << "\n\n큐브는 : " << std::endl;
+    std::transform(std::begin(data), std::end(data),
+        std::ostream_iterator<double>(std::cout, " "), [](double x) { return x * x * x; });
+
+
+    // std::function<> 타입의 변수를 인수로 사용하기
+    std::function<double(double)> op{ [](double x) {return x * x; } };
+    std::cout << "\n\n사각형은 : " << std::endl;
+    std::transform(std::begin(data), std::end(data),
+        std::ostream_iterator<double>(std::cout, " "), op);
 }
 
