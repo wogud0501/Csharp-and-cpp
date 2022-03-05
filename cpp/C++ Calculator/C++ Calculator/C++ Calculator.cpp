6d804af16283b4ca6ca625a3d1edfa1786f63581
Jae-Hyoung Park <djaeyeo@gmail.com>
2022-03-05 13:15:37 +0900

    console C++ Calculator update

---
+++ cpp/C++ Calculator/C++ Calculator/C++ Calculator.cpp
@@ -0,0 +1,15 @@
+﻿// 스트림 반복자 사용하기
+#include <numeric>                                       // accumulate() 함수 - 범위에 속한 원소들의 합계
+#include <iterator>                                      // istream_iterator
+#include <iostream>                                      //
+
+int main()
+{
+    std::cout << "값은 공백으로 구분해서 입력하세요. 종료하려면 Ctrl+Z를 입력하세요:" << std::endl;
+
+    std::cout << "\n입력한 값의 합계는 "
+        << std::accumulate(std::istream_iterator<double>(std::cin),
+            std::istream_iterator<double>(), 0.0)
+        << std::endl;
+}
+
