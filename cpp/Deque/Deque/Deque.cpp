697976ef80721d00cea1e142a0d9433c84b1ccea
Jae-Hyoung Park <djaeyeo@gmail.com>
2022-03-07 19:37:07 +0900

    Source Code Update

--- cpp/Deque/Deque/Deque.cpp
+++ cpp/Deque/Deque/Deque.cpp
@@ -10,5 +10,7 @@ using std::string;
 int main()
 {
     std::deque<string> names;
-    std::cout << "enter first names separated by spaces. Enter Ctrl + Z on a new line to end:\n";
+    std::cout << "첫번째 이름은 공백으로 구분해서 넣으세요. 새 줄에서 Ctrl + Z 를 입력하여 입력을 마치세요:\n";
+    std::copy(std::begin(names), std::end(names), std::ostream_iterator<string> {std::cout, " "});
+    std::cout << std::endl;
 }
