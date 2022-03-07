82a6f218bcdde92b5cf3ed41802a0e841d19d061
Jae-Hyoung Park <djaeyeo@gmail.com>
2022-03-07 19:35:20 +0900

    Source Code Update

--- cpp/Deque/Deque/Deque.cpp
+++ cpp/Deque/Deque/Deque.cpp
@@ -4,7 +4,11 @@
 #include <string>
 #include <iterator>
 
+
+using std::string;
+
 int main()
 {
-    
+    std::deque<string> names;
+    std::cout << "enter first names separated by spaces. Enter Ctrl + Z on a new line to end:\n";
 }
