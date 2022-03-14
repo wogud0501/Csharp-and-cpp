54999830d42600461600119d2861e494f6205872
Jae-Hyoung Park <djaeyeo@gmail.com>
2022-03-14 09:21:33 +0900

    C++ Update

---
+++ cpp/List2/List2/List2.cpp
@@ -0,0 +1,9 @@
+﻿#include <algorithm>
+#include <iostream>
+#include <forward_list>
+#include <iterator>
+#include "Box.h"
+int main()
+{
+    std::cout << "Hello World!\n";
+}
---
+++ cpp/순방향 반복자/순방향 반복자/Range.h
@@ -0,0 +1,31 @@
+template <typename T>
+class Numeric_Iterator : public std::iterator <std::forward_iterator_tag, T>
+{
+	friend class Numeric_Range <T>;
+
+private:
+	Numeric_Range<T>& range;
+	T value;
+
+public:
+	explicit Numeric_Iterator(Numeric_Range<T>& a_range) : range{ a_range }, value{ a_range.start } {};
+
+	Numeric_Iterator& operator=(const Numeric_Iterator& src)
+	{
+		range = src.range;
+		value = src.value;
+	}
+
+	T& operator*()
+	{
+		if (value == static_cast<T>(range.start + range.count * range.step))
+		{
+			throw std::logic_error("Can't dereference an end iterator.");
+		}
+		return value;
+		Numeric_Iterator& operator++()
+		{
+			if(value == static_cast)
+		}
+	}
+};
