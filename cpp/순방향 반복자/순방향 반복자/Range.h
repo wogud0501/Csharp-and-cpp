4ebf011191afe756d95fc56dad45e387d00806ac
Jae-Hyoung Park <djaeyeo@gmail.com>
2022-03-14 17:30:44 +0900

    Update

--- cpp/순방향 반복자/순방향 반복자/Range.h
+++ cpp/순방향 반복자/순방향 반복자/Range.h
@@ -1,37 +1,10 @@
-template <typename T>
-class Numeric_Iterator : public std::iterator <std::forward_iterator_tag, T>
-{
-	friend class Numeric_Range <T>;
+#ifndef NUMERIC_RANGE_H
+#define NUMERIC_RANGE_H
 
-private:
-	Numeric_Range<T>& range;
-	T value;
+#include <exception>
+#include <iterator>
+#include <type_traits>
 
-public:
-	explicit Numeric_Iterator(Numeric_Range<T>& a_range) : range{ a_range }, value{ a_range.start } {};
+template <typename T> class Numeric_Iterator
 
-	Numeric_Iterator& operator=(const Numeric_Iterator& src)
-	{
-		range = src.range;
-		value = src.value;
-	}
-
-	T& operator*()
-	{
-		if (value == static_cast<T>(range.start + range.count * range.step))
-		{
-			throw std::logic_error("Can't dereference an end iterator.");
-		}
-		return value;
-		Numeric_Iterator& operator++()
-		{
-			if (value == static_cast<T>(range.start + range.count * range.step))
-			{
-				throw std::logic_error("Can't increment an end iterator.");
-			}
-			auto temp = *this;
-			value += range.step;
-			return temp;
-		}
-	}
-};
+#endif
