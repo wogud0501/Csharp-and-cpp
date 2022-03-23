4744036893e2451726ca4a9b090e6ebba2493997
Jae-Hyoung Park <djaeyeo@gmail.com>
2022-03-23 19:23:49 +0900

    Update

--- cpp/C++ Calculator/C++ Calculator/C++ Calculator.cpp
+++ cpp/C++ Calculator/C++ Calculator/C++ Calculator.cpp
@@ -8,7 +8,7 @@ int main()
     std::cout << "값은 공백으로 구분해서 입력하세요. 종료하려면 Ctrl+Z를 입력하세요:" << std::endl;
 
     std::cout << "\n입력한 값의 합계는 "
-        << std::accumulate/erator<double>(), 0.0)
+        << std::accumulate.iterator<double>(), 0.0;)
         << std::endl;
 }
 
