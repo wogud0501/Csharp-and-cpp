ea5f64a2bb5a471d28c7852a63eba4125c6776e6
Jae-Hyoung Park <djaeyeo@gmail.com>
2022-04-19 12:50:26 +0900

    C Update

--- cpp/C++ Calculator/C++ Calculator/stdio.c
+++ cpp/C++ Calculator/C++ Calculator/stdio.c
@@ -1,7 +1,11 @@
 #include <stdio.h>;
 
 int main(void) {
-	printf("Hello, World!\n");
-	printf("Hello, World~1\n");
+	printf("월\t화\t수\t목\t금\t토\t일");
+	printf("\n1\t2\t3\t4\t5\t6\t7");
+	printf("\n8\t9\t10\t11\t12\t13\t14");
+	printf("\n15\t16\t17\t18\t19\t20\t21");
+	printf("\n22\t23\t24\t25\t26\t27\t28");
+	printf("\n29\t30\t31");
 	return 0;
 }
