ad071f8df9c64f8358f03ad0c3431571bf58b56f
a <pjh5238@naver.com>
2022-10-07 12:08:03 +0900

    Update

--- C/C/C.c
+++ C/C/C.c
@@ -4,18 +4,16 @@
 #pragma warning ( disable : 4996 )
 #pragma warning ( disable : 4474 )
 
-int main()
-{
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = 0; j < 5; j++)
-		{
-			if (j < i)
-				printf(" ");
-			else
-				printf("*");
+int main() {
+	int num = 5;
+	for (int i = 0; i < num; i++) {
+		for (int j = num - 1; j > i; j--) {
+			printf(" ");
+		}
+
+		for (int j = 0; j < 2 * i + 1; j++) {
+			printf("*");
 		}
 		printf("\n");
 	}
-	return 0;
 }
