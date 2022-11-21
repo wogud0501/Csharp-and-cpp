586d242507663dd89cac8f49d711f5460f436ff8
a <pjh5238@naver.com>
2022-11-21 09:28:25 +0900

    Just update.

--- C/C/C.c
+++ C/C/C.c
@@ -5,11 +5,27 @@
 #pragma warning ( disable : 4474 )
 
 int main() {
-	int i = 0;
-	char d[21];
-	scanf("%s", d);
-	for (i = 0; d[i] != '\0'; i++)
-	{
-		printf("\'%c\'\n", d[i]);
+	int data[10] = { 10, 6, 7, 9, 3, 4, 2, 1, 5, 8 };
+	int temp;
+	for (int i = 0; i < 10; i++) {
+		printf("%d ", data[i]);
 	}
+	printf("\n");
+
+	for (int i = 0; i < 9; i++) {
+		for (int j = i + 1; j < 10; j++) {
+			if (data[i] > data[j]) {
+				temp = data[i];
+				data[i] = data[j];
+				data[j] = a;
+			}
+		}
+	}
+
+	printf("정렬 후 순서\n");
+	for (int i = 0; i < 10; i++) {
+		printf("%d ", data[i]);
+	}
+	printf("\n");
+	return 0;
 }
